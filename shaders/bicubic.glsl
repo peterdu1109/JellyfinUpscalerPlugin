37981ed7771c77bc56e4d71a364d8f2f3d035ee9
Manfred21231 <mahveen.amarachukwu@moneychair.org>
2024-12-27 21:43:27 +0100

    Initial commit: Jellyfin Upscaler Plugin

---
+++ shaders/bicubic.glsl
@@ -0,0 +1,7 @@
+precision highp float;
+varying vec2 vTexCoord;
+uniform sampler2D uTexture;
+void main() {
+    vec4 color = texture2D(uTexture, vTexCoord);
+    gl_FragColor = color;
+}
---
+++ shaders/bilinear.glsl
@@ -0,0 +1,6 @@
+precision highp float;
+varying vec2 vTexCoord;
+uniform sampler2D uTexture;
+void main() {
+    gl_FragColor = texture2D(uTexture, vTexCoord);
+}
